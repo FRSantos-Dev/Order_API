c223e15049d8cc601e9716d1a8677bc61dba1f34
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-10-15 12:47:27 -0300

    Enums created

---
+++ API_Pedido/src/main/java/br/com/rocha/API_Pedido/domain/enums/Perfil.java
@@ -0,0 +1,25 @@
+package br.com.rocha.API_Pedido.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+@Getter
+@AllArgsConstructor
+public enum Perfil {
+
+    ADMIN(1, "ROLE_ADMIN"),
+    CLIENTE(2, "ROLE_CLIENTE");
+
+    private int cod;
+    private String descricao;
+
+    public static Perfil toEnum(Integer cod) {
+        return EnumSet.allOf(Perfil.class).stream()
+                .filter(e -> e.getCod() == (cod))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Id inválido" + cod));
+    }
+
+}
---
+++ API_Pedido/src/main/java/br/com/rocha/API_Pedido/domain/enums/TipoCliente.java
@@ -0,0 +1,25 @@
+package br.com.rocha.API_Pedido.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+@Getter
+@AllArgsConstructor
+public enum TipoCliente {
+
+    PESSOAFISICA(1, "Pessoa Física"),
+    PESSOAJURIDICA(2, "Pessoa Juridica");
+
+    private int cod;
+    private String descricao;
+
+    public static TipoCliente toEnum(Integer cod) {
+        return EnumSet.allOf(TipoCliente.class).stream()
+                .filter(e -> e.getCod() == (cod))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Id inválido" + cod));
+    }
+
+}
